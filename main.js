d81249edd15172ae606ab29f10cb4ca5a5df6d15
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-03-12 22:58:37 +0530

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+// main.js
+console.log("script is available");
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+// main.js
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
+
+setTimeout(() => {
+  window.close();
+}, 1000);
+// // main.js
+
+// window.addEventListener("appinstalled", () => {
+//   disableInAppInstallPrompt();
+// });
+
+// function disableInAppInstallPrompt() {
+//   installPrompt = null;
+//   installButton.setAttribute("hidden", "");
+// }
